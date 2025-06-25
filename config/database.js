e90465c91fa70a5ea1421f38a520c61fc8f6d49e
shoaib khan <82094195+shoaibkhan188626@users.noreply.github.com>
2025-06-26 05:00:26 +0530

    added hospital schema and a custom made logger alsoo database connection

---
+++ config/database.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import logger from "./logger.js";
+
+export const connectDB = async () => {
+  const uri =
+    process.env.NODE_ENV === "production"
+      ? process.env.MONGO_URI_ATLAS
+      : process.env.MONGO_URI_LOCAL;
+  if (!uri) {
+    throw new Error("MongoDB URI is not defined in environment variables");
+  }
+
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    logger.info("Connected to MONGODB");
+  } catch (error) {
+    logger.error(`MongoDB connection error:${error.message}`);
+    throw error;
+  }
+};
---
+++ models/hospital.js
@@ -0,0 +1,74 @@
+import mongoose from "mongoose";
+
+const hospitalSchema = new mongoose.Schema({
+  externalId: {
+    type: String,
+    uniqure: true,
+    required: true,
+    default: () => new mongoose.Types.ObjectId().toString(),
+  },
+
+  name: {
+    type: String,
+    required: true,
+    maxLength: 200,
+    trim: true,
+  },
+
+  address: {
+    strees: { type: String, maxLength: 200, trim: true },
+    city: { type: String, maxLength: 100, trim: true },
+    state: { type: String, maxLength: 200, trim: true },
+    pincode: {
+      type: String,
+      match: /^\d{6}$/,
+    },
+  },
+
+  location: {
+    type: {
+      type: String,
+      enum: ["Point"],
+      default: "Point",
+    },
+    coordinates: {
+      type: [Number],
+      index: "2dsphere",
+    },
+  },
+
+  contact: {
+    phone: {
+      type: String,
+      match: /^[6-9]\d{9}$/,
+      trim: true,
+    },
+    email: {
+      type: String,
+      match: /.+@.+\..+/,
+      trim: true,
+    },
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+
+  deleted: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+hospitalSchema.pre("save", function (next) {
+  this.updatedAt = Date.now();
+  next();
+});
+
+export default mongoose.model("Hospital", hospitalSchema);
