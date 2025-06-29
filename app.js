342246d3d80405e875bbe6407f75a9d1bd1da328
shoaib khan <82094195+shoaibkhan188626@users.noreply.github.com>
2025-06-29 17:44:34 +0530

    added app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+import express from "express";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+import cors from "cors";
+import loggerMiddleware from "./middlewares/logger.middleware.js";
+import errorHandler from "./middlewares/ErrorHandler.js";
+import hospitalRoutes from "./routes/hospital.routes.js";
+
+const app = express();
+
+app.use(helmet());
+
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: "too many requests please try again later",
+  })
+);
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+  })
+);
+
+app.use(express.json());
+app.use(loggerMiddleware);
+app.get("/health", (req, res) => res.status(200).json({ staus: "OK" }));
+app.use("/api/hospitals", hospitalRoutes);
+app.use(errorHandler);
+export default app;
